31be90fa6ff87772dba7d35b671f90f4c180c87c
ToByłemJa <TomashDotCom@gmail.com>
2024-11-16 15:08:46 +0100

    init service

---
+++ ConwaysGameOfLife.py
@@ -0,0 +1,25 @@
+def count_neighbours( neighbours ):
+    return  sum( neighbours )
+
+def is_alive(  was_alive,  num_neighbours  ):
+    if  was_alive:        return 2 <= num_neighbours <= 3
+    else         :        return      num_neighbours == 3
+
+def get_ei8ht_neighbours(  board,  ii,  jj  )-> list :
+	result = []
+	for  x  in  board[  ii-1  ][  jj-1  :  jj+2        ]:        result.append(x)
+	for  x  in  board[  ii    ][  jj-1  :  jj+2  :  2  ]:        result.append(x)
+	for  x  in  board[  ii+1  ][  jj-1  :  jj+2        ]:        result.append(x)
+	return result
+
+world_size_X,  world_size_Y  = 10, 10
+world = [     [ 0 for x in range( world_size_X ) ]
+          for         y in range( world_size_Y )    ]
+
+world[5][5] = 1
+world[5][6] = 1
+world[5][7] = 1
+
+# print( True == 1 )
+# print( True is 1 )
+""" SyntaxWarning:   "is" with a literal.   Did you mean  "==" ?    print( True is 1 )"""
---
+++ ConwaysGameOfLife_test.py
@@ -0,0 +1,39 @@
+import unittest  ### testy jednostek
+import ConwaysGameOfLife
+
+class TestGameOfLife( unittest.TestCase ):
+
+    def test_count_neighbours(self):
+        neighbours = [  0, 1, 0, 1,   0, 1, 0, 0  ]
+        self.assertEqual(  3,  ConwaysGameOfLife.count_neighbours( neighbours )  )
+
+    def test_is_alive(self):       # def is_alive(  was_alive,  num_neighbours  )
+        self.assertFalse( ConwaysGameOfLife.is_alive(  True,  0  ) ) # stop life
+        self.assertFalse( ConwaysGameOfLife.is_alive(  True,  1  ) ) # stop life
+        self.assertFalse( ConwaysGameOfLife.is_alive(  True,  4  ) ) # stop life
+
+        self.assertFalse( ConwaysGameOfLife.is_alive(  False, 2  ) ) # remain death
+        self.assertFalse( ConwaysGameOfLife.is_alive(  False, 4  ) ) # remain death
+
+        self.assertTrue ( ConwaysGameOfLife.is_alive(  True,  2  ) ) # STAY ALIVE
+        self.assertTrue ( ConwaysGameOfLife.is_alive(  True,  3  ) ) # STAY ALIVE
+        self.assertTrue ( ConwaysGameOfLife.is_alive(  False, 3  ) ) # BORN TO LIFE
+
+
+    def test_get_ei8ht_neighbours(self):
+        board    = [   [1, 1, 0],
+                       [0, 9, 1],
+                       [0, 0, 0]   ]
+        expected = [    1, 1, 0 ,
+                        0,    1 ,
+                        0, 0, 0    ]
+        self.assertEqual(  expected,  ConwaysGameOfLife.get_ei8ht_neighbours( board, 1, 1 )  )
+    def test_get_ei8ht_neighbours__lewy(self):
+        board    = [   [2873641, "asdfsjdf", 3],  [4, 5, 6],  [7, 8, 9]   ]
+        expected = [    2873641, "asdfsjdf", 3 ,   4,    6 ,   7, 8, 9    ]
+        self.assertEqual(  expected,  ConwaysGameOfLife.get_ei8ht_neighbours( board, 1, 1 )  )
+
+
+
+if __name__ == "__main__":
+    unittest.main()
